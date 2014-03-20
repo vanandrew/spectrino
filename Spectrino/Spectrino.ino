3d57631df77198012726d435e203fdede245f624
vanandrew <vanandrew77@gmail.com>
2014-03-20 18:39:43 -0500

    Updated Code/Rearranged Files

--- Spectrino/Spectrino.ino
+++ Spectrino/Spectrino.ino
@@ -1,70 +1,93 @@
 /* 
-  This code is for the Spectrino project, a world health application of the arduino board to make a spectrophotometer
+  This code is for the Spectrino project,
+  a world health application of the Arduino board to make a
+  spectrophotometer.
   Written by Andrew Van, Jeffrey Smith, and Claire Kalkbrenner
 */
 
-#include <FHT.h> // Include Fast Hartley Transform Library
+// Variables
+boolean temp_bool;
+boolean data[1024];
+int index1 = -1;
+int index2 = -1;
+int index3 = -1;
+float frequency;
+double samp_period;
+double freq_reading_count = 0;
+double period_sum = 0;
+double freq_avg;
 
-uint16_t freq;
-
+// Initialize Here
 void setup()
 {
-  DDRD = DDRD | B00000000; //Set all  pins(0 to 7) to read mode
-  Serial.begin(9600); // Setup serial connection with 9600 bps baud rate
-  Serial.println("Begin Stream");
+	DDRD = DDRD | B00000000; //Set all  pins(0 to 7) to read mode
+	Serial.begin(9600); // Setup serial connection with 9600 bps baud rate
+	Serial.println("Begin Stream");
 }
 
-void loop()
+void loop(
 {
-  while(1) // Create indefinite while loop for stable loop
-  {
-    delay(1000);
-    for(int i = 0; i < FHT_N; i++) // Read in signal; 256 sample window
-    {
-      fht_input[i] = ((int) bitRead(PIND,2))*255; // Stores an int value in the fht array    
-      delayMicroseconds(10); // Sample every 10 microseconds
-    }
+while(1)
+{ 
+	//Delay allows us to set frequency at which we read values
+	//Store square signal into data
+	for(int i=0; i<1024;i++)
+	{
+	delayMicroseconds(1);
+	data[i] = bitRead(PIND,2);
+	} 
+  
+	temp_bool = data[0];
+	for(int i = 0; i<1024;i++)
+	{
+		// Compares current index with past stored index; Store value is opposite
+		if(temp_bool != data[i])
+		{
+		  // Stores the first index if not already stored
+		  if(index1 == -1){
+			index1 = i;
+			temp_bool = data[i];
+		  }
+		  // Stores the second index if not already stored
+		  else if(index2 == -1){
+			 index2 = i;
+			 temp_bool = data[i];
+		  }
+		  else if(index3 == -1){
+			 index3 = i;
+			 temp_bool = data[i];
+		  }
+		  
+		}   
     
-    for(int m= 0; m < (FHT_N); m++)
-    {
-      Serial.print(fht_input[m]);
-      Serial.print(",");
-    }
-    Serial.println();
-    
-    fht_reorder(); // Reorder data for fht
-    fht_run(); // Run the fht on data
-    fht_mag_lin(); // Find magnitude of data
-   
-    for(int n = 0; n < (FHT_N/2); n++)
-    {
-      Serial.print((float) fht_lin_out[n]);
-      Serial.print(",");
-    }
-    Serial.println();
-    
-    freq = max_of_array(fht_lin_out)*100000/FHT_N;
-    
-    Serial.println(freq);
-  }
-}
+		//Calculate frequency based on index locations
+		if(index1 != -1 && index2 != -1 && index3 != -1)
+		{
+			freq_reading_count++;
+			samp_period = (double)(index3-index1);
+			Serial.print("This is index3: ");
+			Serial.println(index3);
+			Serial.print("This is index1: ");
+			Serial.println(index1);
+			
+			period_sum+=samp_period;
+			index1 = -1;
+			index2 = -1;
+			index3 = -1;
+		}
+  
+	}//end of for loop 
+  
+	// Wait 1 second
+	delay(1000);
+  
+	// Calculate the average frequency of the signal
+	freq_avg = (double)(freq_reading_count/period_sum)*1000;
+	Serial.print("This is freq_avg: ");
+	Serial.println(freq_avg);
 
-// Finds max of windowed array
-uint16_t max_of_array(uint16_t a[])
-{
-  uint16_t temp_max = a[1];
-  uint16_t k;
-  
-  for(uint16_t i = 1; i<(FHT_N/2);i++)
-  {
-    if(temp_max < a[i])
-      uint16_t k = i;
-  }
-  
-  if(temp_max==0)
-    k = 0;
-  
-  Serial.println(k);
-    
-  return k;
+	// Reset variables for next loop
+	freq_reading_count = 0; 
+	period_sum = 0;
+}
 }
