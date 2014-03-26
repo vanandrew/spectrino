025453182f86e1549fa8343d38c32b36adb71bde
vanandrew <vanandrew77@gmail.com>
2014-03-26 11:13:36 -0500

    Create TouchscreenFunctions.h

---
+++ Spectrino/TouchscreenFunctions.h
@@ -0,0 +1,108 @@
+char touchscreen_input()
+{
+    while(1)
+    // Update with most recent coordinates
+    get_coordinates();
+        // Blank
+        if ()
+        {
+            return 'b';
+        }
+        
+        // Standard
+        else if ()
+        {
+            return 's';
+        }
+        
+        // Clear
+        else if ()
+        {
+            return 'c';
+        }
+        
+        // Measure
+        else if()
+        {
+            return 'm';
+        }
+        
+        // Reset
+        else if()
+        {
+            return 'r';
+        }
+        
+        // Enter
+        else if()
+        {
+            return 'e';
+        }
+        
+        // 0
+        else if()
+        {
+            return '0';
+        }
+        
+        // 1
+        else if()
+        {
+            return '1';
+        }
+        
+        // 2
+        else if()
+        {
+            return '2';
+        }
+        
+        // 3
+        else if()
+        {
+            return '3';
+        }
+        
+        // 4
+        else if()
+        {
+            return '4';
+        }
+        
+        // 5
+        else if()
+        {
+            return '5';
+        }
+        
+        // 6
+        else if()
+        {
+            return '6';
+        }
+        
+        // 7
+        else if()
+        {
+            return '7';
+        }
+        
+        // 8
+        else if()
+        {
+            return '8';
+        }
+        
+        // 9
+        else if()
+        {
+            return '9';
+        }
+        
+        // Period
+        else if()
+        {
+            return '.';
+        }
+    }
+}
